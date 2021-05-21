0735a913f755fd761002a403d366734632ff2e16
Anmol-Prasad <84094254+Anmol-Prasad@users.noreply.github.com>
2021-05-21 15:33:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side !!', appName: 'Natours' });
-// });
-// app.post('/', (req, res) => {
-//   res.send('This is a post..');
-// });
-
-// app.get('/api/v1/tours', (req, res) => {
-//   res.status(200).json({
-//     status: 'success',
-//     results: tours.length,
-//     data: {
-//       tours: tours,
-//     },
-//   });
-// });
-
-// co
 const express = require('express');
 const app = express();
 const fs = require('fs');
 app.use(express.json());
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`,'utf-8')
+  fs.readFileSync(
+    `${__dirname}/starter/dev-data/data/tours-simple.json`,
+    'utf-8'
+  )
 );
 
-app.post('/api/v1/tours', (req, res) => {
-  const newid = tours[tours.length - 1].id + 1;
-  console.log(newid);
-  // const newtour = Object.assign({ id: newid }, req.body);
+app.get('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params);
+  const id = req.paramas.id * 1;
+  const tour = tours.find((el) => el.id === id);
 
-  // const tourist=tours.push(newtour);
-
-  // fs.writeFile(
-  //   `${__dirname}/starter/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tourist),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tours: newtour,
-  //       },
-  //     });
-  //   }
-  // );
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
 });
+
+// app.post('/api/v1/tours', (req, res) => {
+//   const newid = tours[tours.length - 1].id + 1;
+//   // console.log(newid);
+//   const newtour = Object.assign({ id: newid }, req.body);
+
+//   tours.push(newtour);
+
+//   fs.writeFile(
+//     `${__dirname}/starter/dev-data/data/tours-simple.json`,
+//     JSON.stringify(tours),
+//     (err) => {
+//       res.status(201).json({
+//         status: 'success',
+//         data: {
+//           tours: newtour,
+//         },
+//       });
+//     }
+//   );
+// });
 
 const port = 3000;
 app.listen(port, () => {
